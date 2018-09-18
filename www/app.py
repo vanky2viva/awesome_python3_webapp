981942506659a42c0941b98717fc22d9d9088516
vanky <ansinmagic@qq.com>
2018-09-18 17:50:22 +0800

    生成配置文件

--- www/app.py
+++ www/app.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+'''
+async web application.
+'''
+
 import logging; logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
@@ -9,25 +16,25 @@ from jinja2 import Environment, FileSystemLoader
 import orm
 from coroweb import add_routes, add_static
 
-def init_jinja2(app,**kw):
+def init_jinja2(app, **kw):
     logging.info('init jinja2...')
-    options  = dict(
+    options = dict(
         autoescape = kw.get('autoescape', True),
         block_start_string = kw.get('block_start_string', '{%'),
         block_end_string = kw.get('block_end_string', '%}'),
         variable_start_string = kw.get('variable_start_string', '{{'),
         variable_end_string = kw.get('variable_end_string', '}}'),
         auto_reload = kw.get('auto_reload', True)
-)
+    )
     path = kw.get('path', None)
     if path is None:
         path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     logging.info('set jinja2 template path: %s' % path)
     env = Environment(loader=FileSystemLoader(path), **options)
-    filters = kw.get('filters',None)
+    filters = kw.get('filters', None)
     if filters is not None:
         for name, f in filters.items():
-            env.filters[name] = f   
+            env.filters[name] = f
     app['__templating__'] = env
 
 async def logger_factory(app, handler):
@@ -45,44 +52,44 @@ async def data_factory(app, handler):
                 logging.info('request json: %s' % str(request.__data__))
             elif request.content_type.startswith('application/x-www-form-urlencoded'):
                 request.__data__ = await request.post()
-                logging.info('request form:%s' %str(request.__data__))
+                logging.info('request form: %s' % str(request.__data__))
         return (await handler(request))
     return parse_data
 
-async def response_factory(app,handler):
+async def response_factory(app, handler):
     async def response(request):
         logging.info('Response handler...')
         r = await handler(request)
-        if isinstance(r,web.StreamResponse):
+        if isinstance(r, web.StreamResponse):
             return r
-        if isinstance(r,bytes):
+        if isinstance(r, bytes):
             resp = web.Response(body=r)
             resp.content_type = 'application/octet-stream'
-            return r
-        if isinstance(r,str):
+            return resp
+        if isinstance(r, str):
             if r.startswith('redirect:'):
                 return web.HTTPFound(r[9:])
             resp = web.Response(body=r.encode('utf-8'))
             resp.content_type = 'text/html;charset=utf-8'
             return resp
-        if isinstance(r,dict):
+        if isinstance(r, dict):
             template = r.get('__template__')
             if template is None:
-                resp = web.Response(body=json.dumps(r,ensure_ascii=False,default=lambda o: o.__dict__).encode('utf-8'))
+                resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
                 resp.content_type = 'application/json;charset=utf-8'
                 return resp
             else:
                 resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
                 resp.content_type = 'text/html;charset=utf-8'
                 return resp
-        if isinstance(r,int) and r >=100 and r < 600:
+        if isinstance(r, int) and r >= 100 and r < 600:
             return web.Response(r)
         if isinstance(r, tuple) and len(r) == 2:
-            t, m =r
+            t, m = r
             if isinstance(t, int) and t >= 100 and t < 600:
-                return web.Response(t,str(m))
-        #default:
-        resp =web.Response(body=str(r).encode('utf-8'))
+                return web.Response(t, str(m))
+        # default:
+        resp = web.Response(body=str(r).encode('utf-8'))
         resp.content_type = 'text/plain;charset=utf-8'
         return resp
     return response
@@ -101,17 +108,17 @@ def datetime_filter(t):
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 async def init(loop):
-    await orm.create_pool(loop=loop,host='127.0.0.1',port=3306,user='www-data', password='wwww-data', db='awesome')
-    app = web.Application(loop=loop,middlewares=[
-        logger_factory,response_factory
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www-data', password='www-data', db='awesome')
+    app = web.Application(loop=loop, middlewares=[
+        logger_factory, response_factory
     ])
-    init_jinja2(app,filters=dict(datetime=datetime_filter))
-    add_routes(app,'handlers')
+    init_jinja2(app, filters=dict(datetime=datetime_filter))
+    add_routes(app, 'handlers')
     add_static(app)
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
-loop = asyncio.get_event_loop() 
+loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
-loop.run_forever
+loop.run_forever()
---
+++ www/test.py
@@ -0,0 +1,2 @@
+import pymysql
+conn = pymysql.connect(host='127.0.0.1',port=3306,user='www-data',password='www-data')
